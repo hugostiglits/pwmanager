8749921241282b700a5b186d9241b46e52e3462d
Hugo Stiglits <zdespusto@gmail.com>
2018-08-03 16:54:22 +1200

    first commit p29

---
+++ app/index.js
@@ -0,0 +1,7 @@
+require('./css/main.css');
+
+var element = document.createElement('h1');
+
+element.innerHTML = "Hello vasil";
+
+document.body.appendChild(element);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+var path = require("path");
+
+var PATHS = {
+    app: path.resolve(__dirname,'app'),
+    build: path.resolve(__dirname,'build')
+};
+
+
+module.exports = {
+  entry: {
+    app: PATHS.app + "/index.js"
+  } ,
+
+  output: {
+    path: PATHS.build,
+    filename: "bundle.js"
+  },
+  devServer: {
+    host: '127.0.0.1',
+    port: 80,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style-loader','css-loader']
+      }
+      {
+        test: /\.js?/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        options: {
+          presets:['env','react']
+        }
+      }
+      {
+        test: /\.png$/,
+        use: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        use: "file-loader"
+      }
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader? limit=10000&mimetype=application/font-woff'
+      },
+       {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+       },
+       {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'file-loader'
+       },
+       {
+         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+         use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+       }
+    ]
+  },
+
+};
